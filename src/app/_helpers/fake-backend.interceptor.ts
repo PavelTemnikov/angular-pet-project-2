efe4a7a9a6160e2c6ac82bbb1bbe8c29e7e8525f
Pavel Temnikov <pashatemnik@gmail.com>
2022-03-16 18:01:13 +0400

    fake-backend.interceptor.ts - init localStorage as fake db

--- src/app/_helpers/fake-backend.interceptor.ts
+++ src/app/_helpers/fake-backend.interceptor.ts
@@ -7,6 +7,32 @@ import {
     HttpResponse,
     HttpErrorResponse
 } from '@angular/common/http';
+interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    refreshTokens: string[];
+}
+
+// init fake data base
+const usersKey = 'jwt-refresh-token-users';
+const rawUsers = localStorage.getItem(usersKey);
+const users: User[] = rawUsers ? JSON.parse(rawUsers) : [];
+
+if (!users.length) {
+    users.push({
+        id: 1,
+        firstName: 'Test',
+        lastName: 'User',
+        username: 'test',
+        password: 'test',
+        refreshTokens: [],
+    });
+    localStorage.setItem(usersKey, JSON.stringify(users));
+}
+
 @Injectable()
 export class FakeBackendInterceptor implements HttpInterceptor {
 
