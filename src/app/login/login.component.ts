87ddcf55b9a8856e421aaa71aa57d501d26399bf
Pavel Temnikov <pashatemnik@gmail.com>
2022-03-17 17:30:05 +0400

    login.component - add template and onSubmit()

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,4 +11,34 @@ import { AuthenticationService } from '../_services/authentication.service';
     styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+    loginForm: FormGroup;
+    errorMessage = '';
+
+    constructor(private _formBuilder: FormBuilder, private _authenticationService: AuthenticationService, private _router: Router) { }
+
+    ngOnInit(): void {
+        this.loginForm = this._formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    get username() { return this.loginForm.controls['username']; }
+    get password() { return this.loginForm.controls['password']; }
+
+    onSubmit() {
+        this._authenticationService.login(this.username.value, this.password.value)
+            .subscribe({
+                next: () => this._router.navigate(['home']),
+                error: (error: HttpErrorResponse | Error) => {
+                    if (error instanceof Error) {
+                        throw error;
+                    }
+                    this.errorMessage = error.error.message;
+                    
+                }
+            });
+    }
+
 }
