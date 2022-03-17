1dd9872dd301f5bcbed29ae138d097e3c2539ca8
Pavel Temnikov <pashatemnik@gmail.com>
2022-03-17 16:51:14 +0400

    add app-routing.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FakeBackendInterceptor } from './_helpers/fake-backend.interceptor';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
+    providers: [
+
         // fake backend
         { provide: HTTP_INTERCEPTORS, useClass: FakeBackendInterceptor, multi: true }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
