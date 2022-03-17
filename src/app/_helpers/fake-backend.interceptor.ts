776de88dde88366d2fe1216cb8a4b93bfab1ad9d
Pavel Temnikov <pashatemnik@gmail.com>
2022-03-17 17:26:37 +0400

    fake-backend.interceptor.ts/authenticate - replace error() to throw Error

--- src/app/_helpers/fake-backend.interceptor.ts
+++ src/app/_helpers/fake-backend.interceptor.ts
@@ -52,7 +52,7 @@ export class FakeBackendInterceptor implements HttpInterceptor {
 
         function authenticate(): Observable<HttpEvent<IUser | null>> {
             if (body === null) {
-                return error('body is null');
+                throw new Error('body is null');
             }
             const { username, password } = body;
             // search in fake db
