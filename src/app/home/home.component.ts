e70367a03725c40e0205186ce940d0087df373dd
Pavel Temnikov <pashatemnik@gmail.com>
2022-03-19 17:01:33 +0400

    home.component.ts - add logic and template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IDbUser } from '../_models';
+import { UserService } from '../_services';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    
+    users$: Observable<IDbUser[]>;
+    test = false;
 
-  constructor() { }
+    constructor(private _userService: UserService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.users$ = this._userService.getAll();
+    }
 
 }
