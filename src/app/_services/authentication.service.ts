ad9cd159e20914349d7db381810a17ca3aa37053
Pavel Temnikov <pashatemnik@gmail.com>
2022-03-19 12:54:21 +0400

    authentication.service.ts - add startRefreshToken()

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -10,6 +10,7 @@ import { IUser } from '../_models';
 })
 export class AuthenticationService {
 
+    private _refreshTokenTimeout: number;
     private _userSubject$ = new BehaviorSubject<IUser | null>(null);
     public user$ = this._userSubject$.asObservable();
 
@@ -31,5 +32,20 @@ export class AuthenticationService {
 
     private _emitUser(user: IUser): void {
         this._userSubject$.next(user);
+        this.startRefreshToken();
+    }
+
+    private startRefreshToken(): void {
+        if (!this.userValue) {
+            return;
+        }
+        const jwtToken: { exp: number } = JSON.parse(
+            atob( 
+                this.userValue.jwtToken.split('.')[1] 
+            )
+        )
+         // set a timeout to refresh the token a minute before it expires
+        const timeout = jwtToken.exp - Date.now() - 1000 * 60;
+        this._refreshTokenTimeout = window.setTimeout(() => this.refreshToken().subscribe(), timeout);
     }
 }
